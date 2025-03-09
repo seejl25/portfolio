71d1f39791a68faeccb5c3d3d9ffa1c59a0f9eb9
seejl25 <seejunlong@u.nus.edu>
2025-03-09 23:01:53 +0800

    Changes made: - Changed layout for projects to Grid - Added in new project "dashboard" - Changed the positions of the "live demo" and "details" button - Added link for "details" - Added glowing animation for profile photo

--- homepage.js
+++ homepage.js
@@ -4,50 +4,58 @@ const projectDescription = document.querySelectorAll(".description");
 
 projectItems.forEach(eachItem => {
     eachItem.addEventListener("mouseenter", () => {
-        if (eachItem.children[1].firstElementChild.textContent === "Calculator") {
+        if (eachItem.children[3].firstElementChild.textContent === "Calculator") {
             const newPara = document.createElement("p");
             newPara.textContent = "A calculator project that tests my understanding of Javascript. Find out more by clicking the buttons!"
-            eachItem.children[1].appendChild(newPara);
+            eachItem.children[3].appendChild(newPara);
             eachItem.addEventListener("mouseleave", () => {
                 newPara.textContent = "";
             })
         }
-        else if (eachItem.children[1].firstElementChild.textContent === "Recipe Webpage") {
+        else if (eachItem.children[3].firstElementChild.textContent === "Recipe Webpage") {
             const newPara = document.createElement("p");
             newPara.textContent = "A recipe website that lets me flaunt my creativity with CSS. Find out more by clicking the buttons!"
-            eachItem.children[1].appendChild(newPara);
+            eachItem.children[3].appendChild(newPara);
             eachItem.addEventListener("mouseleave", () => {
                 newPara.textContent = "";
             })
         }
-        else if (eachItem.children[1].firstElementChild.textContent === "Rock Paper Scissors Game") {
+        else if (eachItem.children[3].firstElementChild.textContent === "Admin Dashboard") {
+            const newPara = document.createElement("p");
+            newPara.textContent = "An admin dashboard layout using CSS Grid. Find out more by clicking the buttons!"
+            eachItem.children[3].appendChild(newPara);
+            eachItem.addEventListener("mouseleave", () => {
+                newPara.textContent = "";
+            })
+        }
+        else if (eachItem.children[3].firstElementChild.textContent === "Rock Paper Scissors Game") {
             const newPara = document.createElement("p");
             newPara.textContent = "A game that tests my logic using Javascript. Find out more by clicking the buttons!"
-            eachItem.children[1].appendChild(newPara);
+            eachItem.children[3].appendChild(newPara);
             eachItem.addEventListener("mouseleave", () => {
                 newPara.textContent = "";
             })
         }
-        else if (eachItem.children[1].firstElementChild.textContent === "Tuition Webpage") {
+        else if (eachItem.children[3].firstElementChild.textContent === "Tuition Webpage") {
             const newPara = document.createElement("p");
             newPara.textContent = "First ever webpage after learning basic HTML and CSS. Find out more by clicking the buttons!"
-            eachItem.children[1].appendChild(newPara);
+            eachItem.children[3].appendChild(newPara);
             eachItem.addEventListener("mouseleave", () => {
                 newPara.textContent = "";
             })
         }
-        else if (eachItem.children[1].firstElementChild.textContent === "Etch-a-sketch") {
+        else if (eachItem.children[3].firstElementChild.textContent === "Etch-a-sketch") {
             const newPara = document.createElement("p");
             newPara.textContent = "A sketch grid that tests my knowledge of flexbox. Find out more by clicking the buttons!"
-            eachItem.children[1].appendChild(newPara);
+            eachItem.children[3].appendChild(newPara);
             eachItem.addEventListener("mouseleave", () => {
                 newPara.textContent = "";
             })
         }
-        else if (eachItem.children[1].firstElementChild.textContent === "Sign-up form") {
+        else if (eachItem.children[3].firstElementChild.textContent === "Sign-up form") {
             const newPara = document.createElement("p");
             newPara.textContent = "A sign-up form that tests my styling skills. Find out more by clicking the buttons!"
-            eachItem.children[1].appendChild(newPara);
+            eachItem.children[3].appendChild(newPara);
             eachItem.addEventListener("mouseleave", () => {
                 newPara.textContent = "";
             })
